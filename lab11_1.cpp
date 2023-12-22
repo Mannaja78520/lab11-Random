90044bfddfd71dc1a4150d0f9b36f2649b2000fe
mannaja <manman7852078520@gmail.com>
2023-12-22 15:39:00 +0700

    lab11_2 complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+
+using namespace std;
+
+void randomGrade(){
+    int random = rand() % 9;
+    string grade[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "You will get " << grade[random] << " in this 261102.";
+}
+
+int main (){
+    srand(time(0));
+    int x;
+    cout << "Press Enter 3 times to reveal your future." << endl;
+    for(int i=0; i < 3; i++){
+        cin.get();
+    }
+    randomGrade();
+}
